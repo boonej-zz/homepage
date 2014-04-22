1c31505110312836615fd23c0668cd67759db740
Jonathan Boone <jb@moonshineinteractive.com>
2014-04-22 16:59:52 -0500

    basic site

---
+++ about-me.md
@@ -0,0 +1,12 @@
+About
+================================================================================
+I am  a mobile application developer specializing in iOS development, API design
+and implementation. I worked for Whole Foods Market, Inc. for 8 years ending my
+career as the lead developer for mobile initiatives. I have developed 
+enterprise mobile applicaitons, designed and implemented several API systems, 
+and led many mobile and desktop based web development projects as a member of 
+a Skunkworks style R&D group within the company. 
+
+Since WFM, I have begun a freelance development career and have cofounded a 
+small mobile studio, Moonshine Interactive. In my free time, I enjoy supporting
+my two boys athletic pursuits, playing music, and fishing.
---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var marked = require('marked');
+var fs = require('fs');
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm:          true,
+  tables:       true,
+  breaks:       false,
+  pedantic:     false,
+  sanatize:     true,
+  smartLists:   true,
+  smartypants:  false
+});
+var about = fs.readFileSync('content/about-me.md', 'utf8');
+var blogs_directory = fs.readdirSync('content/blog');
+var blogs = [];
+for (var i = 0; i != blogs_directory.length; ++i){
+  var blog_raw = fs.readFileSync('content/blog/' + blogs_directory[i], 'utf8');
+  var blog = marked(blog_raw);
+  blogs.push(blog);
+}
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  var about_parsed = marked(about);
+  res.render('index', { 
+    title: 'Jonathan E. Boone',
+    description: 'Mobile Software Engineer',
+    blogs: blogs,
+    about: about_parsed
+  });
+});
+ 
+
+module.exports = router;
